f66c44fd27b1be0a622ce071cc107409d93c5c5c
vladatiglau <57870904+vladatiglau@users.noreply.github.com>
2020-03-19 18:54:02 +0200

    Create Client.java
    Am creat clasa Client care contine 2 paramentri si un constructor.

---
+++ Client.java
@@ -0,0 +1,12 @@
+public class Client {
+
+	private int cnp;
+	private String numeClient;
+	
+	public Client(int cnp, String numeClient) {
+		super();
+		this.cnp = cnp;
+		this.numeClient = numeClient;
+	}
+	
+}
