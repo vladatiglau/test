8c4bdc3418bb6a555a1fb08f43aa5f884441444b
vladatiglau <57870904+vladatiglau@users.noreply.github.com>
2020-03-19 18:42:47 +0200

    Create incerc.java

---
+++ incerc.java
@@ -0,0 +1,7 @@
+public class TestGit {
+    public static void main(String[] args) {
+       
+            System.out.println("Hello Git! Denumirea proiectului de licenta este Sistem software pentru managementul relatiilor cu clientii");
+        }
+       
+}
